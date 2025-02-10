253771a4ea736a6d82bcfbee4f594f805ee23b6d
haeeb <hazeerahmanmvr@gmail.com>
2025-02-10 10:07:19 +0530

    Router Config Changed

--- src/main.jsx
+++ src/main.jsx
@@ -10,9 +10,9 @@ import { BrowserRouter } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <Suspense>
+  // <Suspense>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </Suspense>,
+  // </Suspense>,
 )
