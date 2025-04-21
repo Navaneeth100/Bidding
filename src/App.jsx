8e5b3a06e820494dbc107cf89b203ce8094ae0dd
Hazeeb88 <hazeebzdco@gmail.com>
2025-04-21 09:50:10 +0300

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,34 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-// import './App.css'
-
 import { CssBaseline, ThemeProvider } from '@mui/material';
 import { useRoutes } from 'react-router-dom';
 import Router from './routes/Router';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { baselightTheme } from "./theme/DefaultColors";
+import { GlobalStyles } from '@mui/system';
 
 function App() {
-
   const routing = useRoutes(Router);
   const theme = baselightTheme;
 
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
+      
+      {/* Global style override */}
+      <GlobalStyles
+        styles={{
+          '@media (min-width:1200px)': {
+            '.MuiContainer-root': {
+              maxWidth: 'none !important',
+            },
+          },
+        }}
+      />
+
       {routing}
       <ToastContainer />
     </ThemeProvider>
   );
 }
 
-export default App
+export default App;
