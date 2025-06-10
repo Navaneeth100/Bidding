c9d1d2a4ecdeb4228b0e3ec9ff0f48ec8ed1f575
haeeb <hazeerahmanmvr@gmail.com>
2025-06-10 10:50:34 +0530

    Mainurl Changed

--- mainurl.js
+++ mainurl.js
@@ -1,2 +1,2 @@
-export const url = "http://69.62.76.53/api";
-export const imgurl = "http://69.62.76.53";
+export const url = "https://api.tahadigroup.com/api";
+export const imgurl = "https://api.tahadigroup.com";
